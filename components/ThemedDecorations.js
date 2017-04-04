f41fc6bc16689ef53db02243426bee8c793a5703
Your Name <you@example.com>
2017-04-04 15:29:24 -0400

    Done.

--- components/ThemedDecorations.js
+++ components/ThemedDecorations.js
@@ -5,11 +5,18 @@ export default class ThemedDecorations extends React.Component{
     return(
       <div>
         {
+          // React.Children.map(this.props.children, (child) => {
+
           this.props.children.map((child) => {
-            return (<div className={this.props.theme}>
-                      {child}
-                    </div>)
+            return (
+              React.cloneElement(child, {className: this.props.theme})
+            )
           })
+          // this.props.children.map((child) => {
+          //   return (<div className={this.props.theme}>
+          //             {child}
+          //           </div>)
+          // })
         }
       </div>
     );
